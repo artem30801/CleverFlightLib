32a83df390e67f462003ee60e40ed6e8c50f787a
Arthur <goldartt@gmail.com>
2019-02-27 17:49:23 +0300

    Change led pin back to 21

--- FlightLib/LedLib.py
+++ FlightLib/LedLib.py
@@ -5,7 +5,7 @@ from rpi_ws281x import *
 
 # LED strip configuration:
 LED_COUNT = 29  # Number of LED pixels.
-LED_PIN = 10  # GPIO pin connected to the pixels (18 uses PWM!) (10 uses SPI /dev/spidev0.0).
+LED_PIN = 21  # GPIO pin connected to the pixels (18 uses PWM!) (10 uses SPI /dev/spidev0.0).
 LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA = 10  # DMA channel to use for generating signal (try 10)
 LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
